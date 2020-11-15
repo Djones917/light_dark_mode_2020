a2269dd4ca045bb91819b0d91209b473ffca2373
Derek <nightwing917@hotmail.com>
2020-11-14 19:53:27 -0700

    adding switch theme function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("connected!");
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Swith Theme Dynamically
+function switchTheme(e) {
+   console.log(e);
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
